d61a1cb7fe1d55ee81aee1969785dbe9b24bfd98
huangjianyong <huangjianyong@shoplineapp.com>
2024-05-15 15:38:02 +0800

    feat: http cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
-    styledComponents: true,
+    styledComponents: true
   },
+  images: {
+    minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+  }
 };
 
 export default nextConfig;
